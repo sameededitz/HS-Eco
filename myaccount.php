c44b4d4fed0e7815008307fddf16a956ac6a9b21
Sameed <sameedhassan22@gmail.com>
2023-12-02 18:40:36 +0500

    included nav and foot in all files

---
+++ myaccount.php
@@ -0,0 +1,211 @@
+<?php
+include_once("include/navbar.php");
+?>
+<!-- /header -->
+<!--content-->
+<div class="container container-240">
+
+    <div class="myaccount">
+        <ul class="breadcrumb v3">
+            <li><a href="#">Home</a></li>
+            <li class="active">My Account</li>
+        </ul>
+        <div class="row flex pd">
+            <div class="account-element bd-7">
+                <div class="cmt-title text-center abs">
+                    <h1 class="page-title v1">Login</h1>
+                </div>
+                <div class="page-content">
+                    <p>Sign in to your account</p>
+                    <form class="login-form" method="post" action="#">
+                        <div class="form-group">
+                            <label>Username or email address <span class="f-red">*</span></label>
+                            <input type="text" id="author" class="form-control bdr" name="comment[author]" value="">
+                            <label>Password <span class="f-red">*</span></label>
+                            <input type="email" id="email" class="form-control bdr" name="comment[email]" value="">
+                        </div>
+                        <div class="flex lr">
+                            <button type="submit" class="btn btn-submit btn-gradient">
+                                Login
+                            </button>
+                            <div class="checkbox checkbox-default">
+                                <input id="remember" type="checkbox" value="yes" class="">
+                                <label for="remember"><span class="chk-span" tabindex="2"></span>Remember me</label>
+                            </div>
+                        </div>
+                    </form>
+                    <a href="#" class="btn-lostpwd spc">Lost your password?</a>
+                </div>
+            </div>
+            <div class="account-element bd-7 e-left">
+                <div class="cmt-title text-center abs">
+                    <h1 class="page-title v1">Register</h1>
+                </div>
+                <div class="page-content">
+                    <p>Create your very own account</p>
+                    <form class="login-form" method="post" action="#">
+                        <div class="form-group">
+                            <label>Username or email address <span class="f-red">*</span></label>
+                            <input type="text" id="author2" class="form-control bdr" name="comment[author]" value="">
+                            <label>Password <span class="f-red">*</span></label>
+                            <input type="email" id="email2" class="form-control bdr" name="comment[email]" value="">
+                        </div>
+                        <div class="flex lr">
+                            <button type="submit" class="btn btn-submit btn-gradient">
+                                Register
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="e-category">
+    <div class="container container-240">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h1 class="cate-title">Featured Products</h1>
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/usb.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/macbook.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/flycam.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h1 class="cate-title">Top Rated Products</h1>
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/samsung.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/headphone2.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/anker.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h1 class="cate-title">Top Selling Products</h1>
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/headphone.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/samsung2.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+                <div class="cate-item">
+                    <div class="product-img">
+                        <a href="#"><img src="img/product/sound.jpg" alt="" class="img-reponsive"></a>
+                    </div>
+                    <div class="product-info">
+                        <h3 class="product-title"><a href="#">Epson Home Cinema 5040UB </a></h3>
+                        <div class="product-price v2"><span>$780.00</span></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="feature">
+    <div class="container container-240">
+        <div class="feature-inside">
+            <div class="feature-block text-center">
+                <div class="feature-block-img"><img src="img/feature/truck.png" alt="" class="img-reponsive"></div>
+                <div class="feature-info">
+                    <h3>Worldwide Delivery</h3>
+                    <p>With sites in 5 languages, we ship to over 200 countries & regions.</p>
+                </div>
+            </div>
+
+            <div class="feature-block text-center">
+                <div class="feature-block-img"><img src="img/feature/credit-card.png" alt="" class="img-reponsive"></div>
+                <div class="feature-info">
+                    <h3>Safe Payment</h3>
+                    <p>Pay with the world’s most popular and secure payment methods.</p>
+                </div>
+            </div>
+
+            <div class="feature-block text-center">
+                <div class="feature-block-img"><img src="img/feature/safety.png" alt="" class="img-reponsive"></div>
+                <div class="feature-info">
+                    <h3>Shop with Confidence</h3>
+                    <p>Our Buyer Protection covers your purchase from click to delivery.</p>
+                </div>
+            </div>
+
+            <div class="feature-block text-center">
+                <div class="feature-block-img"><img src="img/feature/telephone.png" alt="" class="img-reponsive"></div>
+                <div class="feature-info">
+                    <h3>24/7 Help Center</h3>
+                    <p>Round-the-clock assistance for a smooth shopping experience.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- / end content -->
+<?php
+include_once("include/footer.php");
+?>
